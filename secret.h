274e90e6184c17fee92c1f878bdafc3d4551dad1
KingSlayer260 <collin.smit@hotmail.com>
2024-01-29 15:42:34 +0100

    Delete secret.h

--- secret.h
+++
@@ -1,8 +0,0 @@
-#ifndef WIFI_H  // Header guards to prevent multiple inclusions
-#define WIFI_H
-
-// data below is set as a define in the file secret.h
-const char* ssid = "Godspot ( wifi van de kerk) "; // mijn thuis wifi omdat we deze opdracht niet meer op school konden maken
-const char* password = "4pp3lT44RT123";
-
-#endif
